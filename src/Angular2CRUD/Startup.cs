961e1468168c25d5043f68f5b686756f082edeb9
Dancho Yordanov <danchosabev@gmail.com>
2016-09-09 15:21:30 +0300

    first commit

--- src/Angular2CRUD/Startup.cs
+++ src/Angular2CRUD/Startup.cs
@@ -10,28 +10,31 @@ using Microsoft.Extensions.Logging;
 
 namespace Angular2CRUD
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			//loggerFactory.AddConsole();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+			//         if (env.IsDevelopment())
+			//         {
+			//             app.UseDeveloperExceptionPage();
+			//         }
+
+			//         app.Run(async (context) =>
+			//         {
+			//             await context.Response.WriteAsync("Hello World!");
+			//         });
+		}
+	}
 }
